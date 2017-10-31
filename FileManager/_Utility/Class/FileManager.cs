e55484f13367b845475f11fbcde9a470f6454a68
Daekhon_jiho-PC\Daekhon_jiho <joe880530@gmail.com>
2017-10-31 18:36:23 +0900

    Program Format design. Save Method design.(Not finish...)

---
+++ FileManager/_Utility/Class/FileManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace FileManager._Utility.Class
+{
+    class FileManager
+    {
+        #region Property Decleration(속성 선언)
+        public string _File_name
+        {
+            //읽기, 쓰기 용도
+            get;
+            set;
+        }
+
+        public string _File_type
+        {
+            //읽기, 쓰기 용도
+            get;
+            set;
+        }
+
+        public string _File_path
+        {
+            //읽기, 쓰기 용도
+            get;
+            set;
+        }
+        #endregion
+        
+        public void save()
+        {
+            try
+            {
+                string _objFullPath = "";
+
+                _objFullPath = _File_path + "\\" + _File_name + string.Format(".{0}", _File_type);
+
+                //#region 파일 타입 분류.
+                //switch (_File_type)
+                //{
+                //    case "txt":
+                //        {
+                //            _objFullPath = _objFullPath + string.Format(".{0}", _File_type);
+                //            Console.WriteLine("Save txt...");
+                //            break;
+                //        }
+                //    case "csv":
+                //        {
+                //            _objFullPath = _objFullPath + string.Format(".{0}", _File_type);
+                //            Console.WriteLine("Save csv...");
+                //            break;
+                //        }
+                //    default:
+                //        {
+                //            Console.WriteLine("Save Default...");
+                //            break;
+                //        }
+                //}
+                //#endregion
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public void delete()
+        {
+            
+        }
+    }
+}
